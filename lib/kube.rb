fb2431fc28585905fb3ba040d67bf3e68af70543
Nathan Kidd <nathankidd@hey.com>
2026-04-20 18:40:05 +0100

    Inline tests into source files with if __FILE__ == $0 guard
    Move all test code from separate test/spec directories into the source
    files they test, guarded by if __FILE__ == $0. Update bin/test to run
    each lib file via bundle exec ruby. Delete test/ and spec/ directories.

---
+++ lib/kube.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'kube/engine'
+
+module Kube
+  def self.engine
+    Engine
+  end
+end
--- lib/kube/engine.rb
+++ lib/kube/engine.rb
@@ -6,3 +6,14 @@ module Kube
   module Engine
   end
 end
+
+if __FILE__ == $0
+  require "bundler/setup"
+  require "minitest/autorun"
+
+  class KubeEngineTest < Minitest::Test
+    def test_version
+      refute_nil Kube::Engine::VERSION
+    end
+  end
+end
